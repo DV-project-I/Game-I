e6dde6b0c817a5174675c9b2fe18e42c16a3fdb0
Onnda <dannoman2004@gmail.com>
2023-10-12 14:42:22 +0200

    fondo de prueba 2

---
+++ Output/Assets/Maps/fondo Dani.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="fondo Dani" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="120" columns="15">
+ <image source="../tileset/Background/fondo Dani.png" width="256" height="144"/>
+</tileset>
