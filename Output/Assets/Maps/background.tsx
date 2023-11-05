11f58c1e5bdb29a7d8139b40fb860938b2c8fcff
Onnda <dannoman2004@gmail.com>
2023-11-05 17:13:44 +0100

    Revert "Updated map"
    This reverts commit c344dd2a8596fa3872651d24abccdb6874c603ad.

--- Output/Assets/Maps/background.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="background" tilewidth="16" tileheight="16" tilecount="3968" columns="31">
- <image source="../tileset/Background/Backgroundlol.png" width="511" height="2048"/>
-</tileset>
