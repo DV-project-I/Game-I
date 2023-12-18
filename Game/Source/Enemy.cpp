1a0f6df220f09775cf6f971e64612143fef29d7b
Onnda <dannoman2004@gmail.com>
2023-12-18 20:08:14 +0100

    ahora c revive en modo enemigo

--- Game/Source/Enemy.cpp
+++ Game/Source/Enemy.cpp
@@ -125,7 +125,8 @@ bool Enemy::Update(float dt) {
 	vel.y = -GRAVITY_Y - 25.2f;
 
 	if (app->scene->player->IsDeath == true) {
-		IsDeath = false;	
+		IsDeath = false;
+		pbody->ctype = ColliderType::ENEMY;
 	}
 
 	if (IsDeath == true) {
