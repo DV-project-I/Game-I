4d38d5ac888a1cdd37b6aad07560457690f1db99
Onnda <dannoman2004@gmail.com>
2023-12-23 12:16:37 +0100

    arbol

---
+++ Game/Source/Tree.h
@@ -0,0 +1,78 @@
+#ifndef __TREE_H__
+#define __TREE_H__
+
+#include "Entity.h"
+#include "Point.h"
+#include "SDL/include/SDL.h"
+#include "Animation.h"
+
+
+struct SDL_Texture;
+
+class Tree : public Entity
+{
+public:
+	
+	Tree();
+	
+	virtual ~Tree();
+
+	bool Awake();
+	
+	bool Start();
+
+	bool Update(float dt);
+
+	bool CleanUp();
+
+	void OnCollision(PhysBody* physA, PhysBody* physB);
+
+	
+
+	void EnemyDeath();
+
+	void SetPosition(int x, int y);
+
+
+
+public:
+	float speed;
+	const char* texturePath;
+	int hp;
+	SDL_Texture* texture = NULL;
+	SDL_Texture* camino = NULL;
+	Animation* currentAnimation = nullptr;
+	PhysBody* pbody;
+	//int pickCoinFxId;
+
+	float timertoplay;
+
+	uint torrentesound;
+	uint torrentesound2;
+
+	PhysBody* ataque;
+
+
+	float movX;
+	float movY;
+	float velY;	
+	bool IsDeath = false;
+	bool pathmode = false;
+
+	bool Ataca;
+
+	Animation IdleAnimIzq;
+	Animation IdleAnimDer;
+	Animation WalkAnimIzq;
+	Animation WalkAnimDer;
+	Animation JumpAnim;
+	Animation AtackAnimDer;
+	Animation AtackAnimIzq;
+	Animation DeathAnim;
+	Animation JumpAnimIzq;
+
+
+
+};
+
+#endif // __TREE_H__
